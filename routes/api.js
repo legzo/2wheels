d06595553b93f50105771e63cc21487bed22d1ff
Julien Terrier <julien.terrier@orange.com>
2017-09-22 22:31:38 +0200

    Adds a method to populate a map of segments and efforts for these segments

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,57 @@
 let express = require('express');
 let router = express.Router();
 let request = require('request');
-var rp = require('request-promise');
+let rp = require('request-promise');
 let colors = require('colors');
-var perfy = require('perfy');
-var logger = require('./logger');
-var config = require('./config.json');
+let perfy = require('perfy');
+let logger = require('./logger');
+let config = require('./config.json');
+
+let ROOT_URL = 'https://www.strava.com/api/v3';
+let PER_PAGE = 40;
+
+router.get('/refresh', function(req, res) {
+  
+  let segments = {};
+  let queries = [];
+  let allEfforts = [];
+
+  getActivities()
+  .then((activities) => {
+    
+    for (var i = 0; i < activities.length; i++) {
+      let activity = activities[i];
+
+      logger.info(`Querying ${activity.name}`);
+
+      queries.push(getActivity(activity.id)
+                    .then((efforts) => allEfforts.push.apply(allEfforts, efforts)));      
+    }
+
+    perfy.start('all-done');
+
+    Promise.all(queries)
+          .then(() => {
+            let elapsed = perfy.end('all-done');
+            logger.info(`All done in ${elapsed.time}`);
+
+            for (var i = 0; i < allEfforts.length; i++) {
+              let effort = allEfforts[i];
+              if(segments[effort.id] == undefined) {
+                segments[effort.id] = [];
+              }
+        
+              segments[effort.id].push(effort.time);
+            }
+            res.send(segments);
+          })
+          .catch((err) => {
+            logger.error('activities could not be fetched'.red, err); 
+            res.send('💩');
+          });
+  }); 
+
+});
 
 router.get('/activities/:id', function(req, res) {
   
@@ -26,7 +72,7 @@ router.get('/activities', function(req, res) {
 });
 
 var getActivity = function(id) {
-  return rp(`https://www.strava.com/api/v3/activities/${id}?access_token=${config.access_token}`)
+  return rp(`${ROOT_URL}/activities/${id}?access_token=${config.access_token}`)
   .then(function(body) {
     
     let activity = JSON.parse(body);
@@ -35,27 +81,27 @@ var getActivity = function(id) {
 
     let efforts = [];
     
-        for (var i = 0; i < segmentEfforts.length; i++) {
-          let segmentEffort = segmentEfforts[i];
-    
-          efforts.push({
-            id: segmentEffort.id,
-            name: segmentEffort.name,
-            time: segmentEffort.elapsed_time
-          });
-        }
+    for (var i = 0; i < segmentEfforts.length; i++) {
+      let segmentEffort = segmentEfforts[i];
+
+      efforts.push({
+        id: segmentEffort.segment.id,
+        name: segmentEffort.name,
+        time: segmentEffort.elapsed_time
+      });
+    }
 
     return new Promise(function(resolve) { 
         resolve(efforts);
     });
   })
   .catch(function (err) {
-    logger.error('activities could not be fetched'.red, err);
+    logger.error('activity could not be fetched'.red, err);
   });  
 }
 
 var getActivities = function() {
-  return rp('https://www.strava.com/api/v3/activities?access_token=' + config.access_token)
+  return rp(`${ROOT_URL}/activities?per_page=${PER_PAGE}&access_token=${config.access_token}`)
   .then(function(body) {
     
     let activities = JSON.parse(body);
@@ -65,6 +111,7 @@ var getActivities = function() {
       let activity = activities[i];
 
       results.push({
+        id: activity.id,
         name: activity.name,
         distance: round(activity.distance / 1000),
         averageSpeed: round(msToKmH(activity.average_speed)),
