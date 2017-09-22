c7e8e0dc6d16fc0ee8010662ab179b91d03079b0
Julien Terrier <julien.terrier@orange.com>
2017-09-22 21:34:27 +0200

    Adds a method for an activity details

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,15 @@ var perfy = require('perfy');
 var logger = require('./logger');
 var config = require('./config.json');
 
+router.get('/activities/:id', function(req, res) {
+  
+  let id = req.params.id;
+  
+  getActivity(id)
+      .then((result) =>  res.send(result));
+
+});
+
 router.get('/activities', function(req, res) {
 
   let params = req.query;
@@ -15,6 +24,35 @@ router.get('/activities', function(req, res) {
     .then((result) =>  res.send(result));
 
 });
+
+var getActivity = function(id) {
+  return rp(`https://www.strava.com/api/v3/activities/${id}?access_token=${config.access_token}`)
+  .then(function(body) {
+    
+    let activity = JSON.parse(body);
+
+    let segmentEfforts = activity.segment_efforts;
+
+    let efforts = [];
+    
+        for (var i = 0; i < segmentEfforts.length; i++) {
+          let segmentEffort = segmentEfforts[i];
+    
+          efforts.push({
+            id: segmentEffort.id,
+            name: segmentEffort.name,
+            time: segmentEffort.elapsed_time
+          });
+        }
+
+    return new Promise(function(resolve) { 
+        resolve(efforts);
+    });
+  })
+  .catch(function (err) {
+    logger.error('activities could not be fetched'.red, err);
+  });  
+}
 
 var getActivities = function() {
   return rp('https://www.strava.com/api/v3/activities?access_token=' + config.access_token)
@@ -28,7 +66,7 @@ var getActivities = function() {
 
       results.push({
         name: activity.name,
-        distance: activity.distance,
+        distance: round(activity.distance / 1000),
         averageSpeed: round(msToKmH(activity.average_speed)),
       });
     }
